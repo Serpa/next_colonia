79b36ba9a4a4253b2de549b09a324b4d1a95f18e
Guilherme Serpa Batista <serpa419@gmail.com>
2022-07-19 19:40:41 -0300

    teste again

--- pages/api/pescadores.js
+++ pages/api/pescadores.js
@@ -5,23 +5,25 @@ const prisma = new PrismaClient();
 
 export default async function pescadores(req, res) {
   const token = await getToken({ req });
-  if (req.method === "GET") {
-    try {
-      const getPescadores = await prisma.pescadores.findMany({
-        where: {
-          nome_colonia: {
-            id: token.user.acess,
+  if (token) {
+    if (req.method === "GET") {
+      try {
+        const getPescadores = await prisma.pescadores.findMany({
+          where: {
+            nome_colonia: {
+              id: token.user.acess,
+            },
           },
-        },
-      });
-      return res.status(200).json(getPescadores);
-    } catch (err) {
-      return res.status(503).json({ err: err.toString(), token });
+        });
+        return res.status(200).json(getPescadores);
+      } catch (err) {
+        return res.status(503).json({ err: err.toString(), token });
+      }
+    } else {
+      return res
+        .status(405)
+        .json({ error: "This request only supports POST requests" });
     }
-  } else {
-    return res
-      .status(405)
-      .json({ error: "This request only supports POST requests" });
   }
 
 };
